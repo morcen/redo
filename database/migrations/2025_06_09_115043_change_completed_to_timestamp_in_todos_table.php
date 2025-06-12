146e17b78a90c6e4156e5ffa22c390176d59a152
Morcen Chavez <morris.chavez@gmail.com>
2025-06-12 15:33:57 +0800

    migration file

---
+++ database/migrations/2025_06_09_115043_change_completed_to_timestamp_in_todos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table("todos", function (Blueprint $table) {
+            $table->dropColumn('completed');
+            $table->timestamp('completed_at')->nullable()->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('todos', function (Blueprint $table) {
+            $table->timestamp('completed')->nullable();
+            $table->dropColumn('completed_at');
+        });
+    }
+};
